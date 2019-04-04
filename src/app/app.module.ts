00e07fc5e6b96a7ee796e0006217b0fbcbdbe533
RBleyenberg <remcobleijenberg@gmail.com>
2019-04-04 11:44:58 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { CustomOverlayContainer } from './theme/utils/custom-overlay-container';
-
 import { AgmCoreModule } from '@agm/core';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
@@ -13,30 +12,22 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   wheelPropagation: true,
   suppressScrollX: true               
 };
-
 import { CalendarModule } from 'angular-calendar';
 import { SharedModule } from './shared/shared.module';
 import { PipesModule } from './theme/pipes/pipes.module';
 import { routing } from './app.routing';
-
 import { AppSettings } from './app.settings';
 import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
 import { BlankComponent } from './pages/blank/blank.component';
-import { SearchComponent } from './pages/search/search.component';
 import { NotFoundComponent } from './pages/errors/not-found/not-found.component';
 import { ErrorComponent } from './pages/errors/error/error.component';
-
-import { TopInfoContentComponent } from './theme/components/top-info-content/top-info-content.component';
 import { SidenavComponent } from './theme/components/sidenav/sidenav.component';
 import { VerticalMenuComponent } from './theme/components/menu/vertical-menu/vertical-menu.component';
 import { HorizontalMenuComponent } from './theme/components/menu/horizontal-menu/horizontal-menu.component';
 import { FlagsMenuComponent } from './theme/components/flags-menu/flags-menu.component';
 import { FullScreenComponent } from './theme/components/fullscreen/fullscreen.component';
-import { ApplicationsComponent } from './theme/components/applications/applications.component';
-import { MessagesComponent } from './theme/components/messages/messages.component';
 import { UserMenuComponent } from './theme/components/user-menu/user-menu.component';
-import { FavoritesComponent } from './theme/components/favorites/favorites.component';
 
 
 @NgModule({
@@ -58,19 +49,14 @@ import { FavoritesComponent } from './theme/components/favorites/favorites.compo
     AppComponent,
     PagesComponent,
     BlankComponent,
-    SearchComponent,
     NotFoundComponent,
     ErrorComponent,
-    TopInfoContentComponent,
     SidenavComponent,
     VerticalMenuComponent,
     HorizontalMenuComponent,
     FlagsMenuComponent,
     FullScreenComponent,
-    ApplicationsComponent,
-    MessagesComponent,
-    UserMenuComponent,
-    FavoritesComponent
+    UserMenuComponent
   ],
   providers: [
     AppSettings,
--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -4,7 +4,7 @@ import { Settings } from './app.settings.model';
 @Injectable()
 export class AppSettings {
     public settings = new Settings(
-        'Dare',   //theme name
+        'Annular',   //theme name
         true,       //loadingSpinner
         true,      //fixedHeader
         true,      //fixedSidenav
